1f1da15f04a7c77048f676d68b596364b048e329
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-01-29 20:21:58 -0600

    Crear el archivo para crear el fondo con js

--- src/components/Fondo.js
+++ src/components/Fondo.js
@@ -1,41 +1,77 @@
-import * as THREE from 'three';
+const fondo = document.createElement('div')
+fondo.classList.add('fondo')
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setAnimationLoop(animate);
-document.body.appendChild(renderer.domElement);
+const COLORES = [
+	'#00ffb2',
+	'#00ffe0',
+	'#02ac97 ',
+	'#00f5ff',
+	'#0cd8e5 ',
+	'#00ffb2',
+	'#00ffe0',
+	'#02ac97 ',
+	'#00f5ff',
+	'#0cd8e5 ',
+]
 
-const verticesOfCube = [
-	-1, -1, -1, 1, -1, -1, 1, 1, -1, -1, 1, -1,
-	-1, -1, 1, 1, -1, 1, 1, 1, 1, -1, 1, 1,
-];
 
-const indicesOfFaces = [
-	2, 1, 0, 0, 3, 2,
-	0, 4, 7, 7, 3, 0,
-	0, 1, 5, 5, 4, 0,
-	1, 2, 6, 6, 5, 1,
-	2, 3, 7, 7, 6, 2,
-	4, 5, 6, 6, 7, 4
-];
 
-const geometry = new THREE.PolyhedronGeometry(verticesOfCube, indicesOfFaces, 6, 2);
-const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-const circle = new THREE.Mesh(geometry, material); scene.add(circle);
-scene.add(circle);
 
-camera.position.z = 5;
 
-function animate() {
+for (let i = 0; i < 10; i++) {
+	const div = document.createElement('div')
+	div.classList.add('circle')
+	div.style.backgroundColor = COLORES[i]
 
-	circle.rotation.x += 0.01;
-	circle.rotation.y += 0.01;
-
-	renderer.render(scene, camera);
-
+	div.style.left = Math.random() * (window.innerWidth - div.offsetWidth) + 'px'
+	div.style.top = Math.random() * (window.innerHeight - div.offsetHeight) + 'px'
+	fondo.appendChild(div)
 }
 
-animate();
+
+document.body.appendChild(fondo)
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	const circles = document.querySelectorAll(".circle");
+
+	let desplazando = false;  // Variable que controla si estamos arrastrando
+
+	// Al hacer clic en el elemento, comenzamos a moverlo
+
+	circles.forEach((elemento) => {
+		elemento.addEventListener('mousedown', (event) => {
+			console.log(event.target)
+			desplazando = true;
+
+			// Guardamos la posición inicial del mouse y el div
+			const offsetX = event.clientX - elemento.offsetLeft;
+			const offsetY = event.clientY - elemento.offsetTop;
+
+			// Función que se ejecuta cuando el mouse se mueve
+			function moverElemento(mouseMoveEvent) {
+				if (desplazando) {
+					// Calculamos la nueva posición del div
+					const nuevaPosX = mouseMoveEvent.clientX - offsetX;
+					const nuevaPosY = mouseMoveEvent.clientY - offsetY;
+
+					// Movemos el div a la nueva posición
+					elemento.style.left = nuevaPosX + 'px';
+					elemento.style.top = nuevaPosY + 'px';
+				}
+			}
+
+			// Evento para mover el elemento con el mouse
+			document.addEventListener('mousemove', moverElemento);
+
+			// Cuando el mouse se suelta, dejamos de mover el elemento
+			document.addEventListener('mouseup', () => {
+				desplazando = false;
+
+				// Dejamos de escuchar el movimiento del mouse
+				document.removeEventListener('mousemove', moverElemento);
+			});
+		});
+	});
+})
