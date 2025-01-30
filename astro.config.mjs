afb1ca60f18785ad367d4da8dc4d5f55dad162c6
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-01-30 11:12:55 -0600

    Añadi el paquete de astro Image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
+    integrations: [image()],
+
 });
