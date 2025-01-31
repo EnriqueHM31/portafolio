2f81d1c3c62477b883d2055dc58b2e0240efd116
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-01-31 17:11:15 -0600

    Agregar tipos

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,11 @@
+
+import HTML from './Iconos/ic_HTML.astro'
+import CSS from './Iconos/ic_CSS.astro'
+import React from './Iconos/ic_React.astro'
+import IconoAstro from './Iconos/ic_Astro.astro'
+import JS from './Iconos/ic_JS.astro'
+import PHP from './Iconos/ic_PHP.astro'
+
 export interface Proyectos {
     id: string;
     data: {
@@ -35,4 +43,23 @@ export interface Data {
     title: string
     images: Images
     tecnologias: object
+}
+
+export interface Componentes {
+    HTML: typeof HTML
+    REACT: typeof React
+    CSS: typeof CSS
+    ASTRO: typeof IconoAstro
+    JS: typeof JS
+    PHP: typeof PHP
+}
+
+/*ETIQUETA */
+export interface Etiqueta {
+    title: string
+    texto: string
+}
+
+export interface Tecnologia {
+    tecnologia: string
 }
