412573c4a594f4c64ed9b13e0cb866b686258c67
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-01-31 12:12:30 -0600

    Acomode los tipos para que conincideran

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,25 @@
+export interface Proyectos {
+    id: string;
+    data: {
+        title: string;
+        tecnologias: Record<string, any>; // Ajusta según la estructura de tus tecnologías
+        images: Record<string, any>; // Ajusta según la estructura de tus imágenes
+    };
+    body: string;
+    filePath: string;
+    digest: string;
+    rendered: {
+        html: string;
+        metadata: Record<string, any>; // Ajusta según la estructura de tus metadatos
+    };
+    collection: string;
+    slug: string;
+    render: () => void; // Cambia el tipo de la función según lo que necesites
+}
+
+type Proyectos_Data = Proyectos[];
+
+
 export interface Proyecto {
     data: Data
     slug: string
