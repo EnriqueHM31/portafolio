27cb7771624a9e82a08fa65d01b32b0a9a2ed3d9
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-01-31 17:11:26 -0600

    Agregar constantes para encapsular

---
+++ src/lib/constantes.ts
@@ -0,0 +1,19 @@
+
+export const DATOS_CONOCEME = [
+    { title: 'Nacionalidad', texto: 'Mexicano' },
+    { title: 'Edad', texto: '21 años' },
+    { title: 'Estado Civil', texto: 'Soltero' },
+    { title: 'Ocupación', texto: 'Estudiante' },
+] as const
+
+export const INFORMACION_HEADER = {
+    title: 'Frontend Developer',
+    descripcion: '"Detrás de cada clic, cada transición, hay un diseño y código frontend trabajando para hacer que todo fluya."',
+} as const
+
+export const LINKS_NAVEGACION = [
+    { nombre: 'Inicio', ruta: '#inicio' },
+    { nombre: 'Proyectos', ruta: '#proyectos' },
+    { nombre: 'Conoceme', ruta: '#conoceme' },
+    { nombre: 'Contacto', ruta: '#contacto' },
+] as const
